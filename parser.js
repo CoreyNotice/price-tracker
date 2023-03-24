0b9a73ea21601bc06490c926ba43d5fc81204321
Corey N <coreynotice@yahoo.com>
2023-03-24 19:17:57 -0400

    enter_url_in_terimal

--- parser.js
+++ parser.js
@@ -1,14 +1,16 @@
 const nightmare=require('nightmare')()
-
+const args=process.argv.slice(2)
+const url=args[0]
+const minPrice=args[1]
 checkPrice()
 async function checkPrice(){
-const priceString= await nightmare.goto("https://www.amazon.com/dp/B0B7CPSN2K")
+const priceString= await nightmare.goto(url)
                                   .wait("#corePrice_feature_div")
                                   .evaluate(()=>document.getElementById("corePrice_feature_div").innerText)
                                   .end()                                
 
 const priceNumber=parseFloat(priceString.replace('$','')) 
-if(priceNumber<100){
+if(priceNumber<minPrice){
     console.log("It is cheap")
 } else{
      console.log("It is expensive")
